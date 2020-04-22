ea60ff4a56fdce6b4d7d67ec103e62a34d869698
Akshay sharma <akshaysharma30121999>
2020-04-22 08:12:43 +0530

    added day15 solution

---
+++ WEEK-3/day15.cpp
@@ -0,0 +1,32 @@
+/*
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is 
+equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array
+ (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra 
+space for the purpose of space complexity analysis.) */
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> result(nums.size(),1);
+        for(int i = 0,temp = 1; i < nums.size(); i++){
+            result[i] *= temp;
+            temp *= nums[i];
+        }
+        for(int i = nums.size()-1,temp = 1; i >=0 ; i--){
+            result[i] *= temp;
+            temp *= nums[i];
+        }
+        return result;
+    }
+};
