f05fd5607aa2fd2269a135ff61712bbcebcb7306
Akshay sharma <akshaysharma30121999>
2020-04-22 08:22:07 +0530

    added day21 solution

---
+++ WEEK-3/day-21.cpp
@@ -0,0 +1,39 @@
+/*
+(This problem is an interactive problem.)
+
+A binary matrix means that all elements are 0 or 1. For each individual row of the matrix,
+ this row is sorted in non-decreasing order.
+
+Given a row-sorted binary matrix binaryMatrix, return leftmost column index(0-indexed) with
+ at least a 1 in it. If such index doesn't exist, return -1.
+
+You can't access the Binary Matrix directly.  You may only access the matrix using a BinaryMatrix 
+interface:
+
+BinaryMatrix.get(x, y) returns the element of the matrix at index (x, y) (0-indexed).
+BinaryMatrix.dimensions() returns a list of 2 elements [m, n], which means the matrix is m * n.
+Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer.
+  Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+For custom testing purposes you're given the binary matrix mat as input in the following four examples.
+ You will not have access the binary matrix directly
+*/
+
+class Solution {
+public:
+   int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        //get the dimensions
+		
+		int n = binaryMatrix.dimensions()[0];
+        int m = binaryMatrix.dimensions()[1];
+		
+		//go as much left as you can, and then go downwards.
+        int check = m-1;
+        for(int i = 0;i<n;i++){
+            while(check>=0 and binaryMatrix.get(i,check)==1){
+                check--;
+            }
+        }
+        return (check==m-1)?-1:check+1;
+    }
+};
